6a969ddf6bf37f4e6ac08ea1d931dc2d3524b7eb
hadidharmawan <hadidharmawan32@gmail.com>
2023-09-03 18:50:59 +0700

    add bcrypt dan authuser pada userService

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,5 +1,11 @@
 import * as User from "../repositories/user.js";
 import {successResponse, errorResponse} from "../utils/response.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const SECRET_ACCESS_TOKEN = 'uk_batch_9';
+const SECRET_REFRESH_TOKEN = 'backend';
 
 export const insertUserData = async (req, res, next) => {
     try {
@@ -8,32 +14,30 @@ export const insertUserData = async (req, res, next) => {
         let password = req.body.password;
         let saltRound = 10;
 
-       let tawa = await User.checkEmailUser(email);
-
+       const [result] = await User.checkEmailUser(email);
+        
        //validasi
         if (name == "" && email == "" && password == ""){
             errorResponse(res, "nama harus diisi");
         }
-        // else if (email == tawa){
-        //     errorResponse(res, "email sudah terdaftar, ubah email ");
-
-        // }
+        else if (result.length > 0){
+            const user = result[0];
+            if (email == user.email){
+                successResponse(res, "email sudah terdaftar, ubah email!");
+            }
+        }
         else{
             bcrypt.hash(password, saltRound, async(err, hash) => {
-
                 const [result] = await User.createUser(name, email,hash);
                 successResponse(res, "data berhasil ditambahkan", {user_id: result.insertId}, 201);
             })
-        }
-        console.log(tawa[email]);
-        
+        } 
     } catch (error) {
         next(error);
     }
 }
 
 export const getAllUserData  = async (req, res, next) => {
-    
     try {
         const [result] = await User.getUser(100);
         
@@ -69,19 +73,39 @@ export const deleteUserData = async (req, res, next) => {
     }
 }
 
-export const checkEmail = async (req, res, next) => {
 
+export const auhtUser = async (req, res, next) => {
+    try {
+        let email = req.body.email;
+        let password = req.body.password;
+        const [result] = await User.checkEmailUser(email);
 
-        try {
-            let email = req.body.email;
-            const emailCheck = await User.checkEmailUser(email);
-          successResponse(res, "Tampilkan", emailCheck.data[0],200);
+        if(result.length > 0){
+            const user = result[0];
+            bcrypt.compare(password, user.password, (error, result) => {
+                if(result){
+                    let claims = {
+                        id: user.user_id,
+                        name: user.name,
+                        email: user.email,
+                    }
+                    let accessToken = jwt.sign(claims, SECRET_ACCESS_TOKEN, {expiresIn:"15m"});
+                    let refreshToken = jwt.sign(claims, SECRET_REFRESH_TOKEN, {expiresIn: "30m"});
+                    let data = {
+                        access_token: accessToken,
+                        refresh_token: refreshToken,
+                    }
 
-        } catch (error) {
-            next(error)
+                    successResponse(response, "success", data);
+                }else {
+                    errorResp(response, "invalid email or password", 401);
+                }
+            });   
+        }else{
+            errorResp(response, "invalid email or password", 401);    
         }
-      
-        
-    
+    } catch (error) {
+        next(error);
+    }
 }
 
